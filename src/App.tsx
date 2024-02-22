8d119bf3b523518704ece1a5142df32e6486f142
Adarsh <adarshshanu3@gmail.com>
2024-02-22 06:47:36 +0530

    some functions created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionCards from './components/QuestionCards';
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameover, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+  const nextQuestion = () => {
+
+  }
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCards
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
